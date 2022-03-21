39a28963380420bdb8aaff220a9ef4e784a397c7
KatherineRen <renshidie@gmail.com>
2022-03-21 14:19:20 -0400

    add pages

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log('server...')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5001
+
+//connect to database
+connectDB()
+
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//create route with express
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' })
+})
+
+//Routes connect userRoutes
+app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
