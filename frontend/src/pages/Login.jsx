cf82fcfb07874bd100aee75a92240b584c7257a0
KatherineRen <renshidie@gmail.com>
2022-03-22 00:25:48 -0400

    state initial

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,81 @@
+import { useState } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
+import { login } from '../features/auth/authSlice'
+
 function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const dispatch = useDispatch()
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+    dispatch(login(userData))
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt />
+          Login
+        </h1>
+        <p>Please log in to get support </p>
+      </section>
+
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              name='email'
+              id='email'
+              value={email}
+              onChange={onChange}
+              required
+              placeholder='Enter your email'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              name='password'
+              id='password'
+              value={password}
+              onChange={onChange}
+              required
+              placeholder='Enter your password'
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
