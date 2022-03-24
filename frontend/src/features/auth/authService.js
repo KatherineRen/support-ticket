0867bb60e82b3d191e7ecaebe93d4977715377df
KatherineRen <renshidie@gmail.com>
2022-03-24 14:40:40 -0400

    login

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -12,12 +12,22 @@ const register = async (userData) => {
   return response.data
 }
 
+//Login user
+const login = async (userData) => {
+  const response = await axios.post(API_URL + '/login', userData)
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+  return response.data
+}
+
 //Logout user
 const logOut = () => localStorage.removeItem('user')
 
 const authService = {
   register,
   logOut,
+  login,
 }
 
 export default authService
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 import { useSelector, useDispatch } from 'react-redux'
-import { login } from '../features/auth/authSlice'
+import { login, reset } from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
+import Spinner from '../components/Spinner'
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -13,9 +15,23 @@ function Login() {
   const { email, password } = formData
 
   const dispatch = useDispatch()
-  const { user, isLoading, isSuccess, message } = useSelector(
+  const navigate = useNavigate()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
     (state) => state.auth
   )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    //success register redirect when logged in
+    if (isSuccess || user) {
+      navigate('/')
+    }
+    dispatch(reset())
+  }, [isError, isSuccess, user, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prev) => ({
@@ -31,6 +47,10 @@ function Login() {
       password,
     }
     dispatch(login(userData))
+  }
+
+  if (isLoading) {
+    return <Spinner />
   }
 
   return (
